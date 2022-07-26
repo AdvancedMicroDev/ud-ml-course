728a23831a8f72f145a1912ae2c27d5a64d4a3b5
GautamP <72156711+AdvancedMicroDev@users.noreply.github.com>
2022-07-26 12:16:49 +0530

    Completed SVM Mini Project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,22 +12,17 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.insert(0, "D:\\ud120-projects\\tools\\")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
 ##############################################################
 # Enter Your Code Here
-
-
-
-##############################################################
+clf = GaussianNB()
 
 ##############################################################
 '''
@@ -36,12 +31,16 @@ The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
 
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
+t0 = time()
+# < your clf.fit() line of code >
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
+t0 = time()
+# < your clf.predict() line of code >
+clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
+acc = clf.score(features_test, labels_test) * 100
+print(f"Final Accuracy: {acc:.4f}%")
 ##############################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,23 +7,33 @@
     Sara has label 0
     Chris has label 1
 """
-    
-import sys
+from sklearn.metrics import accuracy_score
 from time import time
-sys.path.append("../tools/")
+import sys
+sys.path.insert(0, "D:\\ud120-projects\\tools\\")
 from email_preprocess import preprocess
+from sklearn import svm
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
 ### your code goes here ###
+clf = svm.SVC(kernel="linear")
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+
+acc = accuracy_score(pred, labels_test) * 100
+print(f"Final Accuracy: {acc:.4f}%")
 #########################################################
 
 #########################################################
@@ -33,7 +43,7 @@ But the Code provided in Quiz has an Indexing issue
 The Code Below solves that issue, So use this one
 '''
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
 
 #########################################################
